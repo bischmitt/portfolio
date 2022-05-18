3089ae34ab2ea2b990f5e5226e6152508b6cbd8d
Bianca Klein Schmitt <bischmitt98@gmail.com>
2022-05-18 18:12:43 -0300

    card text / clipboard email / snackbar ok

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) {}
+
+  openSnackBar() {
+    this.snackBar.open('Email bischmitt98@gmail.com copiado com sucesso!', undefined,{duration: 3000, verticalPosition:'top', panelClass: ['pink-snackbar']})
+  }
 
   ngOnInit(): void {
   }
+
+  value =`bischmitt98@gmail.com`;
 
 }
