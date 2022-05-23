8bee3b65d2435e4a1dbf1455ee2821f770a52c60
Bianca Klein Schmitt <bischmitt98@gmail.com>
2022-05-23 15:55:10 -0300

    trying to deploy for the second time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 const routes: Routes = [
-  {path:'home', component:HomeComponent},
+  {path: '', redirectTo: '/home', component:HomeComponent},
   {path:'projects', component:ProjectsComponent},
-  {path:'navbar', component:NavbarComponent}
 ];
 
 @NgModule({
